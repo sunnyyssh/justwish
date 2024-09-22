e3a33373f98407d1e727319b9df45fb0ab7e9116
sunnyyssh <blit228@mail.ru>
2024-09-22 15:42:34 +0300

    Implemented sign-out endpoint.

---
+++ src/backend/Justwish.Users/Justwish.Users.Application/User/Handlers/SignOutHandler.cs
@@ -0,0 +1,18 @@
+﻿using Justwish.Users.Domain;
+
+namespace Justwish.Users.Application;
+
+public sealed class SignOutHandler : ICommandHandler<SignOutCommand>
+{
+    private readonly IJwtService _jwtService;
+
+    public SignOutHandler(IJwtService jwtService)
+    {
+        _jwtService = jwtService;
+    }
+    
+    public async Task Handle(SignOutCommand request, CancellationToken cancellationToken)
+    {
+        await _jwtService.InvalidateRefreshTokenAsync(request.Token);
+    }
+}
--- src/backend/Justwish.Users/Justwish.Users.Domain/CQRS/ICommand.cs
+++ src/backend/Justwish.Users/Justwish.Users.Domain/CQRS/ICommand.cs
@@ -2,4 +2,6 @@
 
 namespace Justwish.Users.Domain;
 
+public interface ICommand : IRequest;
+
 public interface ICommand<out TResponse> : IRequest<TResponse>;
