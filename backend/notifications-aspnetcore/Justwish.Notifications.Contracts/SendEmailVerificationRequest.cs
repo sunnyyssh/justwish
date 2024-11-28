7ccb31b64be886a7c47a638de2a4e52f3d0c65bb
sunnyyssh <blit228@mail.ru>
2024-11-28 14:39:20 +0300

    Deleted stupid asp net core app

--- backend/notifications-aspnetcore/Justwish.Notifications.Contracts/SendEmailVerificationRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿using MassTransit;
-
-namespace Justwish.Notifications.Contracts;
-
-[EntityName("send_email_verification")]
-[MessageUrn("send_email_verification")]
-public sealed record SendEmailVerificationRequest(string Email, int Code);
